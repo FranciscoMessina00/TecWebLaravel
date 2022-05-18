53a56738c80863d35bfd716b1a5c5ca66a9dc362
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-05-18 21:37:53 +0200

    Completata pagina faq

---
+++ app/Models/Resources/Faq.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Faq extends Model{
+    public $timestamps = false;
+    protected $primaryKey = 'faqId';
+    
+}
---
+++ database/migrations/2022_05_18_190508_faqs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Faqs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faqs', function(Blueprint $table)
+        {
+            $table->bigIncrements('faqId');
+            $table->string('question');
+            $table->string('answer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
