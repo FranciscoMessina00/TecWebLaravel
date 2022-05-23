ba2bf0cd6a81c0224a304d318391f9e9ab1ba5c9
FranciscoMessina00 <91835899+FranciscoMessina00@users.noreply.github.com>
2022-05-23 23:24:47 +0200

    Modifica navbar
    La scritta di benvenuto ora è messa meglio

--- resources/views/layouts/_nav_public.blade.php
+++ resources/views/layouts/_nav_public.blade.php
@@ -1,10 +1,10 @@
 @php
-    $route=Route::currentRouteName();
+$route=Route::currentRouteName();
 
-    if(Auth::check())
-    {
-        $role = Auth::user()->role;
-    }
+if(Auth::check())
+{
+$role = Auth::user()->role;
+}
 
 @endphp
 
@@ -12,16 +12,18 @@
 <div class="tm-header">
     <div class="container-fluid">
         <div class="tm-header-inner">                    
-            <div class="tm-logo-img-container">
-                <img src="{{asset('/files/FLAK_logo.png')}}" alt="logo" style="display: none"/>
+            <div class="contenitore-flex">
+                <div class="tm-logo-img-container">
+                    <img src="{{asset('/files/FLAK_logo.png')}}" alt="logo" style="display: none"/>
+                </div>
+
+                <!<!-- Messaggio di benvenuto di prova. -->
+                @if(Auth::check())
+                    <p class="pad-tb-mid pad-lr-small">Benvenut* {{Auth::user()->name}}({{Auth::user()->role}})<p>
+                @endif
             </div>
-            
-            <!<!-- Messaggio di benvenuto di prova. -->
-            @if(Auth::check())
-                <p class="pad-tb-mid">Benvenut* {{Auth::user()->name}}({{Auth::user()->role}})<p>
-            @endif
-            
-            
+
+
             <!-- navbar -->
             <nav class="navbar tm-main-nav">
 
@@ -31,66 +33,66 @@
 
                 <div class="collapse navbar-toggleable-sm" id="tmNavbar">
                     <ul class="nav navbar-nav">
-                        
+
                         <li class="nav-item {{ $route == 'home' ? 'active' : '' }}">
                             <a href="{{ route('home') }}" class="nav-link">Home</a>
                         </li>
                         <li class="nav-item {{ $route == 'catalog' ? 'active' : '' }}">
                             <a href="{{ route('catalog') }}" class="nav-link">Catalogo</a>
                         </li>
-                        
+
                         @if(!Auth::check())
-                            @if($route=='login')
-                                <li class="nav-item">
-                                    <div class="tm-btn tm-btn-gray text-white no-select nav-link">Non hai un account?</div>
-                                </li>
-                                <li class="nav-item active">
-                                    <a href="{{ route('register') }}" class="tm-btn nav-link">Registrati</a>
-                                </li>
-                                @elseif($route=='register')
-                                <li class="nav-item">
-                                    <div class="text-white tm-btn tm-btn-gray no-select nav-link">Hai già un account?</div>
-                                </li>
-                                <li class="nav-item active">
-                                    <a href="{{ route('login') }}" class="tm-btn nav-link">Accedi</a>
-                                </li>
-                                @else
-                                <li class="nav-item {{ $route == 'login' ? 'active' : '' }}">
-                                    <a href="{{ route('login') }}" class="nav-link">Accedi</a>
-                                </li>
-                                <li class="nav-item {{ $route == 'register' ? 'active' : '' }}">
-                                    <a href="{{ route('register') }}" class="nav-link">Registrati</a>
-                                </li>
-                            @endif
+                        @if($route=='login')
+                        <li class="nav-item">
+                            <div class="tm-btn tm-btn-gray text-white no-select nav-link">Non hai un account?</div>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="{{ route('register') }}" class="tm-btn nav-link">Registrati</a>
+                        </li>
+                        @elseif($route=='register')
+                        <li class="nav-item">
+                            <div class="text-white tm-btn tm-btn-gray no-select nav-link">Hai già un account?</div>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="{{ route('login') }}" class="tm-btn nav-link">Accedi</a>
+                        </li>
                         @else
-                            <li class="nav-item {{ $route == 'account' ? 'active' : '' }}">
-                                <a href="{{ route('account') }}" class="nav-link">Account</a>
-                            </li>
-                        
-                            @switch($role)
-                                @case('locator')
-                                    <li class="nav-item {{ $route == 'locator.messages' ? 'active' : '' }}">
-                                        <a href="{{ route('locator.messages') }}" class="nav-link">Messaggi</a>
-                                    </li>
-                                    <li class="nav-item {{ $route == 'my-accomodations' ? 'active' : '' }}">
-                                        <a href="{{ route('my-accomodations') }}" class="nav-link">I miei alloggi</a>
-                                    </li>
-                                    @break
-                                @case('student')
-                                    <li class="nav-item {{ $route == 'student.messages' ? 'active' : '' }}">
-                                        <a href="{{ route('student.messages') }}" class="nav-link">Messaggi</a>
-                                    </li>
-                                    @break
-                                @case('admin')
-                                    <li class="nav-item {{ $route == 'admin.stats' ? 'active' : '' }}">
-                                        <a href="{{ route('admin.stats') }}" class="nav-link">Statistiche</a>
-                                    </li>
-                                    @break
-                            @endswitch
-                            
-                            <li class="nav-item {{ $route == 'logout' ? 'active' : '' }}">
-                                <a href="{{ route('logout') }}" class="nav-link">Logout</a>
-                            </li>
+                        <li class="nav-item {{ $route == 'login' ? 'active' : '' }}">
+                            <a href="{{ route('login') }}" class="nav-link">Accedi</a>
+                        </li>
+                        <li class="nav-item {{ $route == 'register' ? 'active' : '' }}">
+                            <a href="{{ route('register') }}" class="nav-link">Registrati</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item {{ $route == 'account' ? 'active' : '' }}">
+                            <a href="{{ route('account') }}" class="nav-link">Account</a>
+                        </li>
+
+                        @switch($role)
+                        @case('locator')
+                        <li class="nav-item {{ $route == 'locator.messages' ? 'active' : '' }}">
+                            <a href="{{ route('locator.messages') }}" class="nav-link">Messaggi</a>
+                        </li>
+                        <li class="nav-item {{ $route == 'my-accomodations' ? 'active' : '' }}">
+                            <a href="{{ route('my-accomodations') }}" class="nav-link">I miei alloggi</a>
+                        </li>
+                        @break
+                        @case('student')
+                        <li class="nav-item {{ $route == 'student.messages' ? 'active' : '' }}">
+                            <a href="{{ route('student.messages') }}" class="nav-link">Messaggi</a>
+                        </li>
+                        @break
+                        @case('admin')
+                        <li class="nav-item {{ $route == 'admin.stats' ? 'active' : '' }}">
+                            <a href="{{ route('admin.stats') }}" class="nav-link">Statistiche</a>
+                        </li>
+                        @break
+                        @endswitch
+
+                        <li class="nav-item {{ $route == 'logout' ? 'active' : '' }}">
+                            <a href="{{ route('logout') }}" class="nav-link">Logout</a>
+                        </li>
                         @endif
                     </ul>                        
                 </div>
@@ -99,4 +101,4 @@
 
         </div>                                  
     </div>            
- </div>
+</div>
