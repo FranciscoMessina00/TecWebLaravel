9305a0528c42973ee53880d746b0558a96163f16
FranciscoMessina00 <91835899+FranciscoMessina00@users.noreply.github.com>
2022-05-24 23:22:02 +0200

    Sistemato css chat
    Ho sistemato alcuni dettagli della chat

--- resources/views/chat/chat_contact.blade.php
+++ resources/views/chat/chat_contact.blade.php
@@ -1,3 +1,3 @@
 <div class="pad-lr-small sfondo-grigio pad-tb-small pad-lr-small bord-rad-mid margin-b-15">
-    <a href="{{route('messages.chat', $contact->userId)}}"><h3>{{$contact->name}} {{$contact->surname}}</h3></a>
+    <a class="text-black" href="{{route('messages.chat', $contact->userId)}}"><h3>{{$contact->name}} {{$contact->surname}}</h3></a>
 </div>
