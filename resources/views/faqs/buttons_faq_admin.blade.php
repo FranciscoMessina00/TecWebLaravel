61f1544896db3ff886faf1431bd09d6949a44adc
FranciscoMessina00 <91835899+FranciscoMessina00@users.noreply.github.com>
2022-05-28 15:07:04 +0200

    Aggiunto elimina faq
    Manca da aggiungere un codice jQuery che cambi i pulsanti quando clicchi su elimina (nella vista si vedono alcune tag li con la classe di stile nascondi)

--- resources/views/faqs/buttons_faq_admin.blade.php
+++ resources/views/faqs/buttons_faq_admin.blade.php
@@ -4,7 +4,16 @@
             <a href="{{route('faq.edit',$faq->faqId)}}" class="tm-btn text-white nav-link margin-b-15">Modifica</a>
         </li>
         <li class="nav-item">
-            <a href="catalogo.html" class="tm-btn tm-btn-brown text-white nav-link margin-b-15">Elimina</a>
+            <a class="tm-btn tm-btn-brown text-white nav-link margin-b-15 no-select">Elimina</a>
+        </li>
+        <li class="nav-item nascondi">
+            <div class="tm-btn tm-btn-brown text-white nav-link margin-b-15 no-select">Sicuro?</div>
+        </li>
+        <li class="nav-item nascondi">
+            <a href="{{route('faq.delete',$faq->faqId)}}" class="tm-btn tm-btn-green text-white nav-link margin-b-15">Si</a>
+        </li>
+        <li class="nav-item nascondi">
+            <div class="tm-btn tm-btn-red text-white nav-link margin-b-15 no-select">No</div>
         </li>
     </ul>
 </div>
