5176829ed14939c63d5c7d28e94fbc22345333f4
FranciscoMessina00 <91835899+FranciscoMessina00@users.noreply.github.com>
2022-06-06 16:07:08 +0200

    Aggiunta icona del sito
    Piccoli dettagli XD, non c'è più quella icona stramba, ora c'è il nostro logo

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -6,9 +6,13 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('icons/site.webmanifest') }}">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+
         @section('scripts')
         <script src="{{ asset('Js/funzioni.js') }}"></script> 
         @show
