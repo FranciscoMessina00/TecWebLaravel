bd531f57a838c76a7019c8e5a36f49c40f5ba812
FranciscoMessina00 <91835899+FranciscoMessina00@users.noreply.github.com>
2022-06-07 11:42:54 +0200

    Aggiunta validazione lato client
    Nella registrazione è stata aggiunta una validazione lato client tramite jQuery. Inoltre nella home è stata rimossa la sezione delle città più rilevanti per rendere il tutto più semplice e perché avremmo dovuto collegare il DB per prendere le varie città e avere una immagine per ogni città inserita, quindi difficile da implementare

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Registrati')
 
 @section('content')
+
+@section('scripts')
+@parent
+<script type="text/javascript" src="{{ asset('Js/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ asset('Js/ValidazioneLatoClient.js') }}"></script>
+@endsection
 <!-- inizio zona di accesso-->
 <br>
 <div class="margin-t-x-large margin-b-40 text-center ">
@@ -13,17 +19,9 @@
         <!-- inizio selezione chi sei-->
         <div>
             <div class="text-left">
-                <!--                    <label for="tipology" style="font-size: x-large;">
-                                        Chi sei?
-                                    </label>-->
                 {{ Form::label('role', 'Chi sei?', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <select id="selettore" name="tipology" class="form-element">
-                    <option value="none" disabled selected>Seleziona un'opzione</option>
-                    <option value="studente">Studente</option>
-                    <option value="locatore">Locatore</option>
-                </select>-->
                 {{ Form::select('role', ['locator' => 'Locatore', 'student' => 'Studente'], null, ['class' => 'form-element','id' => 'tipology']) }}
                 @if ($errors->first('tipology'))
                 <ul class="errors">
@@ -43,13 +41,9 @@
 
 
             <div class="text-left">
-                <!--                    <label for="name" style="font-size: x-large;">
-                                        Nome
-                                    </label>-->
                 {{ Form::label('name', 'Nome', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                    <input class="form-element" id="nome" type="text" name="name" placeholder="Inserisci nome">-->
                 {{ Form::text('name', '', ['class' => 'form-element', 'id' => 'name', 'placeholder'=>'Inserisci nome']) }}
                 @if ($errors->first('name'))
                 <ul class="errors">
@@ -62,14 +56,10 @@
         </div>
         <div>
             <div class="text-left">
-                <!--                <label for="surname" style="font-size: x-large;">
-                                    Cognome
-                                </label>-->
                 {{ Form::label('surname', 'Cognome', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <input class="form-element" id="cognome" type="text" name="surname" placeholder="Inserisci cognome">-->
-                {{ Form::text('surname', '', ['class' => 'form-element', 'id' => 'name', 'placeholder'=>'Inserisci cognome']) }}
+                {{ Form::text('surname', '', ['class' => 'form-element', 'id' => 'surname', 'placeholder'=>'Inserisci cognome']) }}
                 @if ($errors->first('surname'))
                 <ul class="errors">
                     @foreach ($errors->get('surname') as $message)
@@ -81,14 +71,10 @@
         </div>
         <div>
             <div class="text-left">
-                <!--                <label for="username" style="font-size: x-large;">
-                                    Username
-                                </label>-->
                 {{ Form::label('username', 'Username', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <input class="form-element" id="username" type="text" name="username" placeholder="Inserisci username">-->
-                {{ Form::text('username', '', ['class' => 'form-element', 'id' => 'name', 'placeholder'=>'Inserisci username']) }}
+                {{ Form::text('username', '', ['class' => 'form-element', 'id' => 'username', 'placeholder'=>'Inserisci username']) }}
                 @if ($errors->first('username'))
                 <ul class="errors">
                     @foreach ($errors->get('username') as $message)
@@ -100,13 +86,9 @@
         </div>
         <div>
             <div class="text-left">
-                <!--                <label for="mail" style="font-size: x-large;">
-                                    E-mail
-                                </label>-->
                 {{ Form::label('email', 'Email', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <input class="form-element" id="mail" type="text" name="mail" placeholder="Inserisci e-mail">-->
                 {{ Form::text('email', '', ['class' => 'form-element','id' => 'email','placeholder'=>'Inserisci e-mail']) }}
                 @if ($errors->first('email'))
                 <ul class="errors">
@@ -119,18 +101,9 @@
         </div>
         <div>
             <div class="text-left">
-                <!--                <label for="gender" style="font-size: x-large;">
-                                    Gender
-                                </label>-->
                 {{ Form::label('gender', 'Gender', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <select id="gender" name="gender" class="form-element">
-                    <option value="none" disabled selected>Seleziona un'opzione</option>
-                    <option value="donna">Donna</option>
-                    <option value="uomo">Uomo</option>
-                    <option value="altro">Altro</option>
-                </select>-->
                 {{ Form::select('gender', ['altro' => 'Altro', 'uomo' => 'Uomo', 'donna' => 'Donna'], 'X', ['class' => 'form-element','id' => 'gender']) }}
                 @if ($errors->first('gender'))
                 <ul class="errors">
@@ -143,13 +116,9 @@
         </div>
         <div>
             <div class="text-left">
-<!--                <label for="data" style="font-size: x-large;">
-                    Data di nascita
-                </label>-->
-                {{ Form::label('date', 'Data di nascita', ['class' => 'text-mid']) }}
+                {{ Form::label('bornDate', 'Data di nascita', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <input class="form-element" id="data" type="date" name="data">-->
                 {{Form::date('bornDate',null, ['class'=>'form-element'])}}
                 @if ($errors->first('bornDate'))
                 <ul class="errors">
@@ -165,13 +134,9 @@
         <!-- inizio pass-->
         <div>
             <div class="text-left">
-<!--                <label for="password" style="font-size: x-large;">
-                    Password
-                </label>-->
                 {{ Form::label('password', 'Password', ['class' => 'text-mid']) }}
             </div>
             <div class="margin-b-40">
-<!--                <input class="form-element" id="pass" type="password" name="password" placeholder="Inserisci password">-->
                 {{ Form::password('password', ['class' => 'form-element', 'id' => 'password','placeholder'=>'Inserisci password']) }}
                 @if ($errors->first('password'))
                 <ul class="errors">
@@ -187,21 +152,16 @@
         </div>
         <div>
             <div class="text-left">
-<!--                <label for="ripeti-pass" style="font-size: x-large;">
-                    Ripeti la password
-                </label>-->
                 {{ Form::label('password-confirm', 'Conferma password', ['class' => 'text-mid']) }}
                 
             </div>
             <div class="margin-b-40">
-<!--                <input class="form-element" id="ripeti-pass" type="password" placeholder="Ripeti la password">-->
                 {{ Form::password('password_confirmation', ['class' => 'form-element', 'id' => 'password-confirm','placeholder'=>'Ripeti la password']) }}
             </div>
         </div>
         <!-- fine pass-->
         <!-- invia dati -->
         <div class="margin-t-small text-center">
-<!--            <input class="tm-btn" type="submit" value="REGISTRATI">-->
             {{ Form::submit('REGISTRATI', ['class' => 'tm-btn']) }}
         </div>
         {{ Form::close() }}
