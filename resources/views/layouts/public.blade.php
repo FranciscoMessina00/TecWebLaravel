152d1cec63b5e46c663cafa6282524bf31a8f300
FranciscoMessina00 <91835899+FranciscoMessina00@users.noreply.github.com>
2022-06-09 10:03:10 +0200

    Cambio nome file
    Cambiato per mantenere un senso il nome del file JS per il menù a tendina quando la pagina è troppo stretta

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -14,7 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         @section('scripts')
-        <script src="{{ asset('Js/funzioni.js') }}"></script> 
+        <script src="{{ asset('Js/menu.js') }}"></script> 
         @show
 
     </head>
